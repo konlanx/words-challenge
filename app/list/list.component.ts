49fcba26db2c8b5553d6e35af3b3e086bf5e0168
Tim <tihansen@uni-bremen.de>
2019-02-01 22:01:10 +0100

    Wordlist in local storage and editable

--- app/list/list.component.ts
+++ app/list/list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { NgModule } from "@angular/core";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { Word } from "./word.module";
+import * as dialogs from "tns-core-modules/ui/dialogs";
+
+const applicationSettings = require("application-settings");
 
 @NgModule({
 	imports: [
@@ -16,15 +19,20 @@ import { Word } from "./word.module";
 })
 export class ListComponent implements OnInit {
 
+	private wordListKey: string = "wordList";
+
 	wordList: Word[];
 	newWord: Word;
 
 	constructor() {
-		this.wordList = [new Word('test'), new Word('more test')];
+		this.wordList = [];
 		this.newWord = new Word('');
 	}
 
 	ngOnInit(): void {
+		let words = applicationSettings.getString(this.wordListKey, "");
+
+		this.wordList = JSON.parse(words);
 	}
 
 	onNewWordChange(args: any) {
@@ -33,11 +41,39 @@ export class ListComponent implements OnInit {
 
 	addWord(): void {
 		if (this.newWord.value === '') {
-			alert('Bitte ausfüllen!');
+			// If word is empty, do nothing
 			return;
 		}
 
 		this.wordList.push(this.newWord);
 		this.newWord = new Word('');
+
+		this.saveWords();
+	}
+
+	saveWords(): void {
+		let words = JSON.stringify(this.wordList);
+		applicationSettings.setString(this.wordListKey, words);
+	}
+
+	onWordListTap($event): void {
+		let index = $event.index;
+		let wordList = this.wordList;
+		let word = wordList[index];
+		let parent = this;
+		dialogs.prompt({
+			title: "Wort bearbeiten",
+			message: "Wort ändern oder löschen",
+			okButtonText: "Speichern",
+			cancelButtonText: "Löschen",
+			defaultText: word.value
+		}).then(function (result) {
+			if(result.result === true) {
+				word.value = result.text;
+			} else {
+				wordList.splice($event.index, 1);
+			}
+			parent.saveWords();
+		});
 	}
 }
